0523b58f7024d78d9575337ba43128e46a08d33a
EkawaD <steo.ederhy@gmail.com>
2024-06-20 12:34:28 +0200

    feat: validators

--- faunatrack/forms.py
+++ faunatrack/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from faunatrack.models import Projet, Observation
+from faunatrack.validators import validate_latitude
 
 class ProjetForm(forms.ModelForm):
     class Meta:
@@ -11,3 +12,14 @@ class ObsForm(forms.ModelForm):
     class Meta:
         model = Observation
         fields = '__all__'
+
+    def clean_latitude(self):
+        latitude = self.cleaned_data.get('latitude')
+        validate_latitude(latitude)
+        return latitude
+    
+    def clean_longitude(self):
+        latitude = self.cleaned_data.get('longitude')
+        if latitude > 150:
+            raise forms.ValidationError("Vous êtes un grand malade !!")
+        return latitude
