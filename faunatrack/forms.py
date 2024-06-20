fe715a0f18366dbd9166231e36c3b41786518d09
EkawaD <steo.ederhy@gmail.com>
2024-06-20 13:38:42 +0200

    feat: some docs

--- faunatrack/forms.py
+++ faunatrack/forms.py
@@ -2,23 +2,30 @@ from django import forms
 from faunatrack.models import Projet, Observation
 from faunatrack.validators import validate_latitude
 
+# Lorsque l'on a une vue Create ou Update, on doit définir une class Form qui hérite de ModelForm
 class ProjetForm(forms.ModelForm):
     class Meta:
-        model = Projet
-        #fields = '__all__' # Tout les champs de mon model sont dans mon formulaire
-        fields = ["titre", "description", "observations"]
+        model = Projet # On précise le model...
+        fields = ["titre", "description", "observations"] # .. et les champs que l'on veut
 
 class ObsForm(forms.ModelForm):
     class Meta:
         model = Observation
-        fields = '__all__'
+        fields = '__all__' # Tout les champs de mon model sont dans mon formulaire
+
 
     def clean_latitude(self):
+        ''' 
+        Pour valider les données d'un formulaire, on surcharge les méthodes
+        clean_xxxx pour valider le champ xxx 
+        On doit toujours retourner la valeur attendue.
+        '''
         latitude = self.cleaned_data.get('latitude')
         validate_latitude(latitude)
         return latitude
     
     def clean_longitude(self):
+        ''' Exemple ou on ajoute une validation sur le formulaire UNIQUEMENT (pas dans le model)'''
         latitude = self.cleaned_data.get('longitude')
         if latitude > 150:
             raise forms.ValidationError("Vous êtes un grand malade !!")
--- faunatrack/views.py
+++ faunatrack/views.py
@@ -6,27 +6,13 @@ from faunatrack.models import Projet, Observation
 from django.urls import reverse_lazy
 from faunatrack.forms import ProjetForm, ObsForm
 
-class ProjetCreate(CreateView):
-    model = Projet 
-    template_name = "projet_create.html"
-    form_class = ProjetForm
-    success_url = reverse_lazy('projet_list') # return redirect('projet_list')
 
-class ProjetUpdate(UpdateView):
-    model = Projet
-    template_name = "projet_update.html"
-    form_class = ProjetForm
-    success_url = reverse_lazy('projet_list')
 
-class ProjetDelete(DeleteView):
-    model = Projet
-    template_name = "projet_delete.html"
-    success_url = reverse_lazy('projet_list')
-
-# Class based view
-# Class HelloWorld:
-# Create your views here.
-
+# Vue sous forme de fonction 
+# Permet de renvoyer ce que l'on veut.
+# Généralement une HTTPResponse (texte)
+# Un template avec un contexte (render)
+# On peut également rediriger vers une autre url (redirect)
 def hello_world(request):
     return HttpResponse('Hello world!')
 
@@ -43,6 +29,22 @@ class ProjetList(ListView):
     # queryset = Projet.objects.filter(titre__icontains="premier")
     template_name = "projet_list.html"
 
+class ProjetCreate(CreateView):
+    model = Projet 
+    template_name = "projet_create.html"
+    form_class = ProjetForm
+    success_url = reverse_lazy('projet_list') # return redirect('projet_list')
+
+class ProjetUpdate(UpdateView):
+    model = Projet
+    template_name = "projet_update.html"
+    form_class = ProjetForm
+    success_url = reverse_lazy('projet_list')
+
+class ProjetDelete(DeleteView):
+    model = Projet
+    template_name = "projet_delete.html"
+    success_url = reverse_lazy('projet_list')
 
 class ObsList(ListView):
     model = Observation
